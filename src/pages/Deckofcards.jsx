68046a69b6b922d6a0fd38609c7fcdc7a08e22a6
Svitlana <rsv0011@gmail.com>
2024-10-16 18:57:07 +0200

    feat: render the card on the page

--- src/pages/Deckofcards.jsx
+++ src/pages/Deckofcards.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { drawCard } from '../api/deckofcards';
 import { useQuery } from '@tanstack/react-query';
 import { v4 as uuidv4 } from 'uuid';
+import Loader from '../components/Loader';
 
 const Deckofcards = () => {
   const { data, refetch, error, isLoading, status, isPreviousData } = useQuery({
@@ -10,17 +11,36 @@ const Deckofcards = () => {
     enabled: false, // Disable automatic refetching
   });
 
+  if (status === 'error') return <ErrorAlert message={error.message} />;
+
+  console.log(data);
+
   return (
     <div>
-      <h1>deck of cards api</h1>
       <button
         onClick={() => {
           refetch();
         }}
+        className="btn my-20"
       >
-        Some Button
+        Draw the card
       </button>
-      ;
+      {isLoading && <Loader />}
+      {data?.cards?.length === 0 && <InfoAlert message={'Please try drawing a card again'} />}
+      {data?.cards?.map((card) => (
+        <div className="flex justify-center" key={uuidv4()}>
+          <div className="card w-80 bg-base-100 shadow-xl">
+            <figure>
+              <img src={card.image} alt="Shoes" />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title justify-center text-primary">
+                {card.value} of {card.suit}
+              </h2>
+            </div>
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
